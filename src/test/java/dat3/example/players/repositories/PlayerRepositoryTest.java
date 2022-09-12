444a1d21b3b48ecf1d3ca1226ab64f4c69ca101f
plaul <lmor@kea.dk>
2022-09-12 11:21:04 +0200

    Update PlayerRepositoryTest.java

--- src/test/java/dat3/example/players/repositories/PlayerRepositoryTest.java
+++ src/test/java/dat3/example/players/repositories/PlayerRepositoryTest.java
@@ -1,13 +1,13 @@
 package dat3.example.players.repositories;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerRepositoryTest {
 
     //@Test
-    void fail(){
+    //void fail(){
        // assertEquals(1,0);
-    }
+    //}
 }
